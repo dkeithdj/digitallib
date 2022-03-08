ee514af69c59381177d386e23cac0e30007ed834
Denrei Keith <denzkei12@gmail.com>
2022-03-08 22:16:04 +0800

    feat: added methods, also has a bit of explanation per method

--- src/main/java/com/dlib/TableOf.java
+++ src/main/java/com/dlib/TableOf.java
@@ -1,36 +1,37 @@
 package com.dlib;
 
-import java.lang.reflect.Array;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
-// import javax.swing.table.DefaultTableModel;
 import javax.swing.table.*;
 
 public class TableOf extends Utils {
-  protected String dbTable;
+  protected String dbTable, id;
   protected String[] col;
-  private JTable mTable;
-  private Connection con;
   private ArrayList<String> head;
-  private ArrayList<String> colName;
-  // private ArrayList<String> outPut;
-  private String id;
-  private ArrayList<String> jtTxts;
+
   // Manage start
   protected JPanel panel;
   protected JScrollPane pane;
   protected JButton addBut, editBut, remBut;
   // Manage end
 
-  protected JTable jtbl;
+  public static JTable bookJTbl, memJTbl, issbookJTbl;
+
+  protected Map<String, String> selectOutPut;
+  protected ArrayList<String> l;
 
   // Constructor
   // @param dbName: name of database
@@ -38,20 +39,6 @@ public class TableOf extends Utils {
   public TableOf(String dbName) {
     this.dbTable = dbName;
     this.head = getTableColName(dbName);
-
-    // JTable jtbl = new JTable();
-    // jtbl.setModel(setupTable());
-    // mTable.getTableHeader().setReorderingAllowed(false);
-    // mTable.setAutoCreateRowSorter(true);
-    // mTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-    // this.headers = headers;
-  }
-
-  // Constructor for managing tables
-
-  // sets dbName to access the database table
-  public void setDBName(String dbName) {
-    this.dbTable = dbName;
   }
 
   // @param sets column names for JTable
@@ -59,199 +46,142 @@ public class TableOf extends Utils {
     this.col = col;
   }
 
-  public void getDbName() {
-    System.out.println(this.dbTable);
-  }
-
-  public JTable getJTable() {
-    return this.mTable;
-  }
-
   public JPanel getPanel() {
     return this.getPanel();
   }
 
-  public ArrayList<String> setEdits(ArrayList<String> colName, String ID, String action) {
-    this.id = ID;
-    this.colName = colName;
-    return this.setEdits(jtTxts, colName, ID, action);
-  }
-
-  public ArrayList<String> setEdits(ArrayList<String> jtTxt, ArrayList<String> colName) {
-    this.jtTxts = jtTxt;
-    this.colName = colName;
-    return this.setEdits(jtTxt, colName, id, "insert");
-  }
-
-  public ArrayList<String> setEdits(ArrayList<String> jtTxt) {
-    this.jtTxts = jtTxt;
-    return this.setEdits(jtTxt, colName, id, "insert");
-  }
-
-  public ArrayList<String> setEdits(ArrayList<String> colName, String action) {
-    this.colName = colName;
-    return this.setEdits(jtTxts, colName, id, action);
-  }
-
-  public ArrayList<String> setEdits(String action) {
-    return this.setEdits(jtTxts, colName, id, action);
-  }
-
-  public ArrayList<String> setEdits(ArrayList<String> jtTxt, ArrayList<String> colName, String action) {
-    this.colName = colName;
-    this.jtTxts = jtTxt;
-    return this.setEdits(jtTxt, colName, id, action);
-  }
-
-  /*
-   * Gets the list of outputs from the database
-   * 
-   * @param jtTxt the data from JTextfields
-   * 
-   * @param colName column names from the database e.g. m_id, b_id, title, etc.
-   * 
-   * @param ID the specific row you want to get based on the ID number
-   * 
-   * @param action can be "select","update","insert"
-   */
-  public ArrayList<String> setEdits(ArrayList<String> jtTxt, ArrayList<String> colName, String ID, String action) {
-    this.colName = colName;
-    this.id = ID;
-    this.jtTxts = jtTxt;
-
+  // jtTxt will be used as values that will be added in the database
+  public boolean insertRow(ArrayList<String> jtTxt) {
     try {
       con = connectToDB();
-      ResultSet rs = null;
 
-      ArrayList<String> qryOut = new ArrayList<String>();
+      // formats the array e.g.
+      // [m_id, firstName, lastName, title] to (firstname,lastName,title)
+      // (\\w+) is a regex expression that selects a word e.g first, getting
+      String base = head.toString();
+      base = base.replaceFirst("(\\w+),", "").replace("[", "(").replace("]", ")").replace(" ", "");
+      String fmt = base.replaceAll("(\\w+)", "?");
+      String qry = "INSERT INTO " + dbTable + base + " VALUES" + fmt;
 
-      String qry = formatQuery(action);
       PreparedStatement pstmt = con.prepareStatement(qry);
-      System.out.println(qry);
       pstmt.executeUpdate("USE library");
 
-      // outputs something
-      if (action.equals("select")) {
-        rs = pstmt.executeQuery(qry);
-        if (rs.next()) {
-          for (int i = 0; i < colName.size(); i++) {
-            qryOut.add(rs.getString(colName.get(i)));
-            // this.outPut = qryOut;
-          }
-        }
-        // self explanatory
-      }
-      // else if (action.equals("delete")) {
-      // pstmt.setString(1, ID);
-      // // inserts or updates stuff to the database
-      // }
-      else {
-        for (int i = 0; i < jtTxt.size(); i++) {
-          if (jtTxt.get(i).isBlank() || jtTxt == null) {
-            return null;
-          }
+      for (int i = 0; i < jtTxt.size(); i++) {
+        if (jtTxt.get(i).isBlank() || jtTxt == null) {
+          return false;
+        } else {
           pstmt.setString(i + 1, jtTxt.get(i));
         }
       }
+
       pstmt.execute();
 
       con.close();
-      return qryOut;
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      return null;
+      return true;
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
   }
 
-  public boolean deleteRow(String ID) {
+  // Updates an existing row in the database with the specific ID
+  public boolean updateRow(ArrayList<String> jtTxt, String ID) {
     try {
       con = connectToDB();
 
-      String qry = "DELETE FROM " + dbTable + " WHERE " + head.get(0) + "=?";
+      // formats the array e.g.
+      // [m_id, firstName, lastName, title] to firstname=?,lastName=?,title=?
+      String base = head.toString();
+      base = base.replaceFirst("(\\w+),", "").replace("[", "").replace("]", "=?").replace(" ", "").replace(",", "=?,");
+      String qry = "UPDATE " + dbTable + " SET " + base + " WHERE " + head.get(0) + "=" + ID;
 
       PreparedStatement pstmt = con.prepareStatement(qry);
       System.out.println(qry);
       pstmt.executeUpdate("USE library");
-      pstmt.setString(1, ID);
 
+      for (int i = 0; i < jtTxt.size(); i++) {
+        if (jtTxt.get(i).isBlank() || jtTxt == null) {
+          return false;
+        } else {
+          pstmt.setString(i + 1, jtTxt.get(i));
+        }
+      }
       pstmt.executeUpdate();
 
-      System.out.println(ID);
-
+      con.close();
       return true;
-
     } catch (Exception e) {
-      // TODO: handle exception
       e.printStackTrace();
       return false;
     }
 
   }
 
-  public ArrayList<String> selectRow(String ID) {
+  // Self-explanatory, this deletes the row based on the ID selected
+  public boolean deleteRow(String ID) {
     try {
       con = connectToDB();
-      ResultSet rs = null;
 
-      ArrayList<String> qryOut = new ArrayList<String>();
+      String qry = "DELETE FROM " + dbTable + " WHERE " + head.get(0) + "=" + ID;
+
+      Statement pstmt = con.createStatement();
+
+      pstmt.executeUpdate("USE library");
+      pstmt.execute(qry);
+
+      con.close();
+      return true;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
+
+  }
+
+  // selects the row of the selected ID
+  // returns a Map in order for easy fetching of data when choosing a value
+  public Map<String, String> selectRow(String ID) {
+    try {
+      con = connectToDB();
+
+      Map<String, String> qryOutPut = new LinkedHashMap<String, String>();
 
       String qry = "SELECT * FROM " + dbTable + " WHERE " + head.get(0) + "=" + ID;
 
       PreparedStatement pstmt = con.prepareStatement(qry);
       System.out.println(qry);
       pstmt.executeUpdate("USE library");
-      rs = pstmt.executeQuery(qry);
-      int i = 0;
-      while (rs.next()) {
-        // for (int i = 0; i < colName.size(); i++) {
-        qryOut.add(rs.getString(head.get(i)));
-        System.out.println(qryOut);
-        i++;
-        // }
+      ResultSet rs = pstmt.executeQuery(qry);
+      if (rs.next()) {
+        for (int i = 0; i < head.size(); i++) {
+          qryOutPut.put(head.get(i), rs.getString(head.get(i)));
+        }
       }
 
       pstmt.execute();
 
-      return qryOut;
+      qryOutPut.remove(head.get(0));
+
+      this.selectOutPut = qryOutPut;
+
+      con.close();
+      return qryOutPut;
     } catch (Exception e) {
-      // TODO: handle exception
       e.printStackTrace();
       return null;
     }
 
   }
 
-  private String formatQuery(String e) {
-    // colName.add(0, head.get(0));
-    // head.remove(0);
-    // String base = head.toString();
-
-    if (e.equals("update")) {
-      String base = colName.toString();
-      base = base.replace("[", "").replace("]", "=?").replace(" ", "").replace(",", "=?,");
-      String fmt = "UPDATE " + dbTable + " SET " + base + " WHERE " + head.get(0) + "=" + id;
-      return fmt;
-
-    }
-    if (e.equals("insert")) {
-      String base = colName.toString();
-      base = base.replace("[", "(").replace("]", ")").replace(" ", "");
-      String fmt = base.replaceAll("(\\w+)", "?");
-      String qry = "INSERT INTO " + dbTable + base + " VALUES" + fmt;
-      return qry;
-    }
-    // if (e.equals("delete")) {
-    // String qry = "DELETE FROM " + dbTable + " WHERE " + head.get(0) + "=?";
-    // return qry;
-    // }
-    if (e.equals("select")) {
-      String qry = "SELECT * FROM " + dbTable + " WHERE " + head.get(0) + "=" + id;
-      return qry;
-    }
-    return "";
-
+  // converts a Map to an arraylist
+  protected ArrayList<String> convertToList(Map<String, String> hashOut) {
+    Collection<String> val = hashOut.values();
+    ArrayList<String> valL = new ArrayList<String>(val);
+    return valL;
   }
 
+  // Gets the table IDs (usually used in JComboBox)
   public ArrayList<String> getTableIDs() {
     ArrayList<String> IDs = new ArrayList<String>();
     try {
@@ -312,15 +242,72 @@ public class TableOf extends Utils {
     return null;
   }
 
-  // Configure JTable's features?
-  public JTable getTable() {
-    JTable mTable = new JTable();
+  // Function that gets the number of rows in the table
+  public int getTableRowNum() {
+    return this.getTableRowNum(dbTable);
+  }
 
-    mTable.setModel(setupTable());
-    mTable.getTableHeader().setReorderingAllowed(false);
-    mTable.setAutoCreateRowSorter(true);
-    mTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-    return mTable;
+  public int getTableRowNum(String tableName) {
+
+    try {
+      con = connectToDB();
+      Statement stmt = con.createStatement();
+      stmt.executeUpdate("USE library");
+      ResultSet rs = stmt.executeQuery("select count(*) from " + tableName);
+      rs.next();
+      return rs.getInt(1);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return 0;
+  }
+
+  // Function that gets the column titles from the database
+  public ArrayList<String> getTableColName(String tableName) {
+    int colCount = 0;
+    ArrayList<String> col = new ArrayList<String>();
+
+    try {
+      con = connectToDB();
+      Statement stmt = con.createStatement();
+      stmt.executeUpdate("USE library");
+
+      ResultSet rs = stmt.executeQuery("SELECT * FROM " + tableName);
+      ResultSetMetaData mtd = rs.getMetaData();
+
+      colCount = mtd.getColumnCount();
+
+      for (int i = 1; i <= colCount; i++) {
+
+        col.add(mtd.getColumnName(i));
+
+      }
+      // return col;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return col;
+  }
+
+  // understandable
+  public String getDateToday() {
+    return this.getDateToday("MM/dd/yyyy");
+  }
+
+  public String getDateToday(String pattern) {
+    DateTimeFormatter dfmt = DateTimeFormatter.ofPattern(pattern);
+    LocalDateTime today = LocalDateTime.now();
+    String dateToday = today.format(dfmt);
+    return dateToday;
+  }
+
+  // Refreshes the tables after there're changes happend such as
+  // adding,editing,removing data
+  // Made it static so that they can share the same value
+  public void refreshTable() {
+    bookJTbl.setModel(new ManageBooks().setupTable());
+    memJTbl.setModel(new ManageMembers().setupTable());
+    issbookJTbl.setModel(new ManageIssBooks().setupTable());
   }
 
 }
