9be9d2060f5474bc624ee2ea5325dc954a6f5d89
Denrei Keith <denzkei12@gmail.com>
2022-03-11 22:25:55 +0800

    feat: background image, reorder layouts

--- src/main/java/com/dlib/AdminMenu.java
+++ src/main/java/com/dlib/AdminMenu.java
@@ -1,10 +1,15 @@
 package com.dlib;
 
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -17,11 +22,34 @@ public class AdminMenu {
 
   private JButton manageDB;
 
+  private ImageIcon img;
+  private Image scaleImg;
+
   public void adminPage() {
     frm = new JFrame("Admin");
     JTabbedPane tab = new JTabbedPane();
 
-    mPnl = new JPanel(new MigLayout("fill, insets 5 5 5 5", "[60%][40%]", "[100%][]"));
+    // gets the image and scales it appropriately
+    img = new ImageIcon("digitalLibSys.png");
+    scaleImg = img.getImage().getScaledInstance(1280, 720, Image.SCALE_SMOOTH);
+    img = new ImageIcon(scaleImg);
+    final JLabel imgLabel = new JLabel(img);
+    imgLabel.setVisible(true);
+
+    // Panel that overrides methods in order for the background image be
+    // displayed
+    mPnl = new JPanel(new MigLayout("fill, insets 5 5 5 5", "[][]", "[90%][][]")) {
+      @Override
+      protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(scaleImg, 0, 0, getWidth(), getHeight(), this);
+      }
+
+      @Override
+      public Dimension getPreferredSize() {
+        return new Dimension(900, 500);
+      }
+    };
 
     manageDB = new JButton("Manage Database");
     manageDB.addActionListener(new ActionListener() {
@@ -30,6 +58,8 @@ public class AdminMenu {
       }
     });
 
+    JLabel aa = new JLabel("<html> </html>");
+
     // Checks if database has already setup
     if (new Utils().hasDatabaseSetup()) {
       // creating TableOf objects to access the getPanel method
@@ -37,15 +67,16 @@ public class AdminMenu {
       TableOf membersTable = new ManageMembers();
       TableOf issueBooksTable = new ManageIssBooks();
 
-      mPnl.add(booksTable.getPanel(), "spany 2, grow");
-      mPnl.add(membersTable.getPanel(), "wrap, grow");
-      mPnl.add(manageDB, "split, right");
+      mPnl.add(aa, "wrap,spanx 2");
+      mPnl.add(issueBooksTable.getPanel(), "wrap,align center");
+      mPnl.add(manageDB, "skip,spanx 2,right");
 
-      tab.add(mPnl, "Books and Members");
-      tab.add(issueBooksTable.getPanel(), "Issued Books");
+      tab.add(mPnl, "Issue Books");
+      tab.add(booksTable.getPanel(), "Books");
+      tab.add(membersTable.getPanel(), "Members");
 
       frm.add(tab);
-      frm.setSize(1000, 500);
+      frm.setSize(900, 500);
       frm.setVisible(true);
       frm.setLocationRelativeTo(null);
       frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
