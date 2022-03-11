86cab5a00acb94ba3d3f6993a46abdcced469ca5
Denrei Keith <denzkei12@gmail.com>
2022-03-11 22:26:25 +0800

    clean: login

--- src/main/java/com/dlib/Main.java
+++ src/main/java/com/dlib/Main.java
@@ -6,8 +6,7 @@ package com.dlib;
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("yes");
-    new AdminMenu().adminPage();
-    // new Login().login();
+    // new AdminMenu().adminPage();
+    new Login().login();
   }
 }
